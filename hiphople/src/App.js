a06779879ab9004ddd08b4d69b5ba70427b3ddee
hkim2 <rlaghekd777@naver.com>
2021-07-13 03:07:22 +0900

    파일 삭제

--- hiphople/src/App.js
+++ hiphople/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
+import Swiper from "./components/Swiper/Swiper";
+
 function App() {
   return (
     <div>
       <Header />
+      <Swiper />
     </div>
   );
 }
