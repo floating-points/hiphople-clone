22261427fb5ffb9dddc9b4cd7fcf6060f0065225
kimho <rlaghekd777@naver.com>
2021-07-14 01:50:24 +0900

    add title

--- hiphople/src/components/Swiper/Swiper.js
+++ hiphople/src/components/Swiper/Swiper.js
@@ -13,15 +13,15 @@ const slides = [
 	},
 	{
 		image: "/images/2.jpg",
-		title: "첫 번째 멍뭉이"
+		title: "두 번째 멍뭉이"
 	},
 	{
 		image: "/images/3.jpg",
-		title: "첫 번째 멍뭉이"
+		title: "세 번째 멍뭉이"
 	},
 	{
 		image: "/images/4.jpg",
-		title: "첫 번째 멍뭉이"
+		title: "네 번째 멍뭉이"
 	}
 ];
 
@@ -40,9 +40,11 @@ const Swiper = () => {
 		{
 			setCurrentIndex(currentIndex + 1);
 			setCurrentPosition(currentPosition - IMAGE_WIDTH)
-			styles = {
-				transform: `translateX(${currentPosition}px)`
-			}
+		}
+		else
+		{
+			setCurrentIndex(0);
+			setCurrentPosition(0);
 		}
 	}
 
@@ -51,9 +53,11 @@ const Swiper = () => {
 		{
 			setCurrentIndex(currentIndex - 1);
 			setCurrentPosition(currentPosition + IMAGE_WIDTH)
-			styles = {
-				transform: `translateX(${currentPosition}px)`
-			}
+		}
+		else
+		{
+			setCurrentIndex(MAX_IMAGE_COUNT);
+			setCurrentPosition(-MAX_IMAGE_COUNT * IMAGE_WIDTH);
 		}
 	}
 
@@ -62,7 +66,10 @@ const Swiper = () => {
 			<div className="container">
 				<div className="slide" style={styles}>
 					{slides.map( (slide) => (
+						<div>
 						<img src={slide.image} />
+						<span className="slide_title">{slide.title}</span>
+						</div>
 					))}
 				</div>
 			</div>
