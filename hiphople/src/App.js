368fbff5174f7245debf4e18882f32cf2e0576e6
kimho <rlaghekd777@naver.com>
2021-07-15 02:12:27 +0900

    add PosterBanner

--- hiphople/src/App.js
+++ hiphople/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from "./components/Header/Header";
+import PosterBanner from './components/PosterBanner/PosterBanner';
 import Swiper from "./components/Swiper/Swiper";
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <div>
       <Header />
       <Swiper />
-
+      <PosterBanner />
     </div>
   );
 }
