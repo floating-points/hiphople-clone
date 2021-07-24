0a471f780d4f0f35ab49d9db3d9d84c60cb12f89
hkim2 <rlaghekd777@naver.com>
2021-07-24 17:35:25 +0900

    add url

--- hiphople/src/components/Header/Sns.js
+++ hiphople/src/components/Header/Sns.js
@@ -9,10 +9,18 @@ import "./sns.scss";
 const Sns = () => {
 	return (
 		<div className="sns">
+            <a href="https://www.instagram.com/" target="_blank" rel="noopenner">
             <IoLogoInstagram className="icon" size="2rem" />
+            </a>
+
+            <a href="https://www.youtube.com/?hl=KO" target="_blank" rel="noopenner">
             <IoLogoYoutube className="icon" size="2rem" />
+            </a>
+
+            <a href="https://m.facebook.com/?locale2=ko_KR" target="_blank" rel="noopenner">
              <IoLogoFacebook className="icon" size="2rem" />
-		</div>
+            </a>
+             </div>
 	)
 }
 
