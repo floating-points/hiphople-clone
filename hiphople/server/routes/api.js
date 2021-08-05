3469f786d157dbfbc78f1261f947f2b25fd0d593
witch-factory <soakdma37@gmail.com>
2021-08-05 20:42:25 +0900

    게시판 양식 통일

--- hiphople/server/routes/api.js
+++ hiphople/server/routes/api.js
@@ -5,14 +5,19 @@ import fashion from "./fashion.js";
 import work from "./work.js";
 import mix from "./mix.js";
 import album from "./album.js";
+import board from "./board.js"
 
 const router=express.Router();
 
+router.use("/:board", (req,res)=>{
+    res.send(req.params.board);
+});
+/*
 router.use("/domestic", domestic);
 router.use("/global", global);
 router.use("/fashion", fashion);
 router.use("/work", work);
 router.use("/mix", mix);
-router.use("/album", album);
+router.use("/album", album);*/
 
 export default router;
