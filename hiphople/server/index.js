c9de2fc29313bf35365e523253e458c8f6bda517
witch-factory <soakdma37@gmail.com>
2021-08-05 21:33:07 +0900

    db 연결까지만 성공

--- hiphople/server/index.js
+++ hiphople/server/index.js
@@ -2,16 +2,31 @@ import express from "express"
 import api from "./routes/api.js"
 import path from "path";
 import bodyParser from "body-parser";
+import mysql from 'mysql';
 
 const __dirname=path.resolve();
 const app=express();
 app.use(express.json());
 
 const PORT=8000;
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api",api);
-app.use("/", express.static(__dirname+"../../client/build"));
+//app.use("/api",api);
+//app.use("/", express.static(__dirname+"../../client/build"));
+
+const conn=mysql.createConnection({
+    host:'localhost',
+    user:'witch',
+    password:'witch'
+});
+
+conn.connect((err)=>{
+    if(err){throw(err);}
+    console.log("connected to DB");
+})
+
+app.get("/", (req,res)=>{
+    res.send("Hello");
+})
 
 app.listen(PORT, ()=>{
     console.log("example app running on port", PORT);
