96817c328acba719cac2ef3871276f529ea2acb4
witch-factory <soakdma37@gmail.com>
2021-08-06 15:30:18 +0900

    mysql 연결과 쿼리 날리기

--- hiphople/server/index.js
+++ hiphople/server/index.js
@@ -11,24 +11,35 @@ app.use(express.json());
 const PORT=8000;
 
 //app.use("/api",api);
-app.use("/", express.static(__dirname+"/client/build"));
+//app.use("/", express.static(__dirname+"/client/build"));
 
-/*const conn=mysql.createConnection({
+const conn=mysql.createConnection({
     host:'localhost',
     user:'witch',
-    password:'witch'
+    password:'witch',
+    database:'domestic'
 });
 
 conn.connect((err)=>{
-    if(err){throw(err);}
-    console.log("connected to DB");
+    if(err){throw err;}
+    console.log("테이블 생성 쿼리 실행");
+    const sqlQuery="create table post(" +
+        "id int not null auto_increment primary key," +
+        "title nvarchar(100) not null," +
+        "content text(5000) not null," +
+        "writer int not null," +
+        "timerecord datetime not null" +
+        ");";
+    conn.query(sqlQuery, (err,result)=>{
+        if(err){throw err ;}
+        console.log("table created");
+    })
 })
 
 app.get("/", (req,res)=>{
     res.send("Hello");
-})*/
+});
 
 app.listen(PORT, ()=>{
     console.log("example app running on port", PORT);
-    console.log(__dirname);
-})
+});
