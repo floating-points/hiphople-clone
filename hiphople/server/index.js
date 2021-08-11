cb2c9919c56720f60070b4853332e63abebbc028
witch-factory <soakdma37@gmail.com>
2021-08-11 21:07:07 +0900

    댓글 db 추가쿼리

--- hiphople/server/index.js
+++ hiphople/server/index.js
@@ -32,29 +32,41 @@ conn.query("create database domestic character set utf8", (err, result)=>{
 });*/
 
 /*테이블 생성 코드
-const tableQuery="create table posts" +
+const postsTableQuery="create table posts" +
   "(id int not null primary key auto_increment," +
-  "title nvarchar(100) not null," +
+  "title nvarchar(100)," +
   "content text(5000)," +
   "writer int not null," +
-  "timerecord datetime)";
+  "timerecord datetime not null);";
 
 conn.query(tableQuery, (err, result)=>{
     if(err){throw err;}
-    console.log("테이블 생성 완료");
+    console.log("게시글 테이블 생성 완료");
 });*/
 
-/* 데이터 하나 추가하는 코드
-const insertQuery="insert into posts(id, title, content, writer, timerecord) values(1, \"테스트 제목3\", \"테스트 내용3\", 2, NOW())";
+const commentsTableQuery="create table comments" +
+    "(id int not null primary key auto_increment," +
+    "post int not null," +
+    "content text(1000)," +
+    "writer int not null," +
+    "timerecord datetime not null);";
+
+conn.query(commentsTableQuery, (err, result)=>{
+    if(err){throw err;}
+    console.log("테이블 생성 완료");
+});
+
+// 데이터 하나 추가하는 코드
+/*const insertQuery="insert into posts(id, title, content, writer, timerecord) values(1, \"테스트 제목3\", \"테스트 내용3\", 2, NOW())";
 
 conn.query(insertQuery, (err, result, fields)=>{
     if(err){throw err;}
     console.log(result);
-});*/
+});
 
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname, "server/form.html"));
-});
+});*/
 
 app.post("/", (req,res)=>{
     const newPostQuery="insert into posts set ?";
