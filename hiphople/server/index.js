1a531d9309640b5402fa8a3ae6c93b96a77d8674
witch-factory <soakdma37@gmail.com>
2021-08-13 16:55:11 +0900

    boardName에 따라 다양한 게시판에 댓글 관련 쿼리 날릴 수 있게 수정

--- hiphople/server/index.js
+++ hiphople/server/index.js
@@ -13,11 +13,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use("/api",api);
 //app.use("/", express.static(__dirname+"/client/build"));
 
-/* DB 생성 코드
-connection.query("create database domestic character set utf8", (err, result)=>{
-    if(err){throw err;}
-    console.log("국내 게시판 DB 생성 완료");
-});*/
+//DB 생성 코드
+//connection.query("create database board character set utf8");
 
 /*테이블 생성 코드
 const postsTableQuery="create table posts" +
@@ -32,17 +29,15 @@ connection.query(tableQuery, (err, result)=>{
     console.log("게시글 테이블 생성 완료");
 });*/
 
-/*const commentsTableQuery="create table comments" +
+//댓글 db 생성 코드
+/*const commentsTableQuery="create table domestic_comments" +
     "(id int not null primary key auto_increment," +
     "post int not null," +
     "content text(1000)," +
     "writer int not null," +
     "timerecord datetime not null);";
 
-connection.query(commentsTableQuery, (err, result)=>{
-    if(err){throw err;}
-    console.log("테이블 생성 완료");
-});*/
+connection.query(commentsTableQuery);*/
 
 // 글 db에 데이터 하나 추가하는 코드
 /*const insertQuery="insert into posts(id, title, content, writer, timerecord) values(1, \"테스트 제목3\", \"테스트 내용3\", 2, NOW())";
@@ -56,40 +51,40 @@ app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname, "server/form.html"));
 });*/
 
-const commentFilteredByPost=async (postID)=>{
-    const commentFilterQuery="select * from comments where post=?";
-    const result=await connection.query(commentFilterQuery, postID);
+const boardCommentFilteredByPost=async (boardName, postID)=>{
+    const commentFilterQuery="select * from "+boardName+" where post=?";
+    const result=await connection.query(commentFilterQuery, [postID]);
     console.log(result[0]);
     return result[0];
 };
 
-const commentUpdate=async (commentID, newContent)=>{
-    const commentUpdateQuery="update comments set content=? where id=?";
+const boardCommentUpdate=async (boardName, commentID, newContent)=>{
+    const commentUpdateQuery="update "+boardName+" set content=? where id=?";
     const result=await connection.query(commentUpdateQuery, [newContent, commentID]);
     console.log("댓글 수정 완료");
 };
 
-const commentInsert=async(postID, writer, content)=>{
-    const commentInsertQuery="insert into comments(post, writer, content, timerecord) values(?,?,?,?)";
+const boardCommentInsert=async(boardName, postID, writer, content)=>{
+    const commentInsertQuery="insert into "+boardName+"(post, writer, content, timerecord) values(?,?,?,?)";
     const currentTime=new Date().toISOString().slice(0, 19).replace("T", " ");
     const result=await connection.query(commentInsertQuery, [postID, writer, content, currentTime]);
     console.log("댓글 삽입 완료");
 };
 
 app.post("/comment-search", async (req,res)=>{
-    const {postID}=req.body;
-    res.send(await commentFilteredByPost(postID));
+    const {boardName, postID}=req.body;
+    res.send(await boardCommentFilteredByPost(boardName, postID));
 });
 
 app.post("/comment-update", async (req,res)=>{
-    const {commentID, newContent}=req.body;
-    res.send(await commentUpdate(commentID, newContent));
+    const {boardName, commentID, newContent}=req.body;
+    res.send(await boardCommentUpdate(boardName, commentID, newContent));
 });
 
 app.post("/comment-insert", async (req,res)=>{
-    const {postID, writer, content}=req.body;
+    const {boardName, postID, writer, content}=req.body;
     //datetime for mysql
-    res.send(await commentInsert(postID, writer, content));
+    res.send(await boardCommentInsert(boardName, postID, writer, content));
 });
 
 app.get("/", async (req, res)=>{
