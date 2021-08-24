f148b15880ea9081084757bf7c671cae2b1e345d
ahnella04 <chqkq96@gmail.com>
2021-08-25 05:12:08 +0900

    Login.js

--- hiphople/client/src/App.js
+++ hiphople/client/src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import { Route, Link } from "react-router-dom";
 import Mainpage from './components/Mainpage/Mainpage';
 import DomesticBoard from './components/DomesticBoard/DomesticBoard';
+import Auth from "./components/Login/Auth/Auth";
 
 function App() {
   return (
-    <div>
-
-      <Route path="/" exact component={Mainpage} />
-      <Route path="/DomesticBoard" component={DomesticBoard} />
-
-    </div>
+    // <div>
+    //   <Route path="/" exact component={Mainpage} />
+    //   <Route path="/DomesticBoard" component={DomesticBoard} />
+    // </div>
+    <Auth />
   );
 }
 
